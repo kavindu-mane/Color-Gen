9517093592ce6ab2ab83b52151d692bfc4a40729
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-06-17 12:31:34 +0530

    Minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -126,96 +126,99 @@ export const Home = () => {
           </h3>
         </section>
 
-        <section className="flex flex-grow items-center justify-center w-full py-10">
-          <div className="flex items-center justify-center max-w-xl w-full">
-            <div className="flex items-center justify-center mx-6 w-full">
-              <div className="relative w-12 h-[2.65rem] mt-[0.05rem] p-1.5 border rounded-bl-lg rounded-tl-lg border-neutral-200 bg-white dark:border-neutral-700 dark:bg-neutral-900">
-                <input
-                  type="color"
-                  value={inputColor}
-                  onChange={handleColorChange}
-                  className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
-                />
+        <main className="px-6 xl:px-0">
+          <section className="flex flex-grow items-center justify-center w-full py-10">
+            <div className="flex items-center justify-center max-w-xl w-full">
+              <div className="flex items-center justify-center mx-6 w-full">
                 <div
-                  style={{ backgroundColor: inputColor }}
-                  className="w-full h-full rounded-full"
-                />
-              </div>
-              <input
-                type="text"
-                value={inputColor.toUpperCase()}
-                onChange={handleTextInputChange}
-                placeholder="#FFFFFF"
-                className="block w-full rounded-l-none rtl:rounded-l-lg rtl:rounded-r-none placeholder-neutral-400/70 dark:placeholder-neutral-500 rounded-lg border border-neutral-200 bg-white px-5 py-2 text-neutral-700 focus:border-neutral-400 focus:outline-none dark:border-neutral-600 dark:bg-neutral-900 dark:text-neutral-300"
-              />
-              <button
-                type="button"
-                onClick={copyPaletteCode}
-                className="ml-3 py-3 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-neutral-950 hover:bg-neutral-800 dark:bg-white dark:hover:bg-gray-300 text-white dark:text-black disabled:opacity-50 disabled:pointer-events-none active:scale-95 transition-transform duration-150"
-              >
-                <FaPaste />
-              </button>
-            </div>
-          </div>
-        </section>
-
-        <section className="flex flex-col items-center justify-center w-full px-6 lg:px-0 pb-10 max-w-3xl">
-          <div className="grid grid-cols-1 sm:grid-cols-11 gap-y-2 lg:gap-x-8 w-full">
-            {Object.entries(palette).map(([shade, colorValue]) => (
-              <div key={shade} className="flex flex-col items-center">
-                <div
-                  className="w-full sm:w-14 lg:w-16 h-16 rounded-lg relative"
-                  style={{ backgroundColor: colorValue }}
-                  onClick={() => handleColorClick(shade)}
-                >
-                  {tooltip[shade] && (
-                    <div className="absolute top-0 left-0 bg-black text-white text-xs p-1 rounded">
-                      Copied
-                    </div>
-                  )}
+                  className="relative w-12 h-[2.65rem] mt-[0.05rem] p-1.5 border rounded-bl-lg rounded-tl-lg border-neutral-200 bg-white dark:border-neutral-700 dark:bg-neutral-900">
+                  <input
+                    type="color"
+                    value={inputColor}
+                    onChange={handleColorChange}
+                    className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+                  />
+                  <div
+                    style={{backgroundColor: inputColor}}
+                    className="w-full h-full rounded-full"
+                  />
                 </div>
-                <p className="mt-2 text-sm text-gray-700 dark:text-gray-300">
-                  {shade}
-                </p>
-                <p className="text-xs text-gray-500 dark:text-gray-400">
-                  {colorValue.toUpperCase()}
-                </p>
-              </div>
-            ))}
-          </div>
-        </section>
-
-        <section className="max-w-7xl">
-          <div className="container px-6 pb-6 m-auto">
-            <div className="grid grid-cols-4 gap-6 lg:grid-cols-12">
-              <div className="col-span-4 lg:col-span-3">
-                <SignOut color={color} />
-              </div>
-              <div className="col-span-4 lg:col-span-6">
-                <ForgotPassword color={color} />
-              </div>
-              <div className="col-span-4 lg:col-span-3">
-                <Cookie color={color} />
+                <input
+                  type="text"
+                  value={inputColor.toUpperCase()}
+                  onChange={handleTextInputChange}
+                  placeholder="#FFFFFF"
+                  className="block w-full rounded-l-none rtl:rounded-l-lg rtl:rounded-r-none placeholder-neutral-400/70 dark:placeholder-neutral-500 rounded-lg border border-neutral-200 bg-white px-5 py-2 text-neutral-700 focus:border-neutral-400 focus:outline-none dark:border-neutral-600 dark:bg-neutral-900 dark:text-neutral-300"
+                />
+                <button
+                  type="button"
+                  onClick={copyPaletteCode}
+                  className="ml-3 py-3 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-neutral-950 hover:bg-neutral-800 dark:bg-white dark:hover:bg-gray-300 text-white dark:text-black disabled:opacity-50 disabled:pointer-events-none active:scale-95 transition-transform duration-150"
+                >
+                  <FaPaste/>
+                </button>
               </div>
             </div>
-          </div>
+          </section>
 
-          <div className="container px-6 pb-6 m-auto">
-            <div className="grid grid-cols-4 gap-6 lg:grid-cols-12">
-              <div className="col-span-4 lg:col-span-7">
-                <Pricing color={color} />
-              </div>
-              <div className="col-span-4 lg:col-span-5">
-                <SwitchNotification color={color} />
+          <section className="flex items-center justify-center w-full pb-10 max-w-3xl mx-auto">
+            <div className="grid grid-cols-1 sm:grid-cols-11 gap-y-2 lg:gap-x-8 w-full">
+              {Object.entries(palette).map(([shade, colorValue]) => (
+                <div key={shade} className="flex flex-col items-center justify-center">
+                  <div
+                    className="w-full sm:w-14 lg:w-16 h-16 relative rounded-lg cursor-pointer transition-colors duration-300 ease-in-out hover:scale-105 hover:shadow-lg"
+                    style={{backgroundColor: colorValue}}
+                    onClick={() => handleColorClick(shade)}
+                  >
+                    {tooltip[shade] && (
+                      <div className="absolute top-0 left-0 bg-black text-white text-xs p-1 rounded">
+                        Copied
+                      </div>
+                    )}
+                  </div>
+                  <p className="mt-2 text-sm text-gray-700 dark:text-gray-300">
+                    {shade}
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    {colorValue.toUpperCase()}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </section>
+
+          <section className="max-w-7xl">
+            <div className="container pb-6 m-auto">
+              <div className="grid grid-cols-4 gap-6 lg:grid-cols-12">
+                <div className="col-span-4 lg:col-span-3">
+                  <SignOut color={color}/>
+                </div>
+                <div className="col-span-4 lg:col-span-6">
+                  <ForgotPassword color={color}/>
+                </div>
+                <div className="col-span-4 lg:col-span-3">
+                  <Cookie color={color}/>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="container px-6 pb-8 m-auto">
-            <Stats color={color} />
-          </div>
-        </section>
-        <Footer />
+            <div className="container pb-6 m-auto">
+              <div className="grid grid-cols-4 gap-6 lg:grid-cols-12">
+                <div className="col-span-4 lg:col-span-7">
+                  <Pricing color={color}/>
+                </div>
+                <div className="col-span-4 lg:col-span-5">
+                  <SwitchNotification color={color}/>
+                </div>
+              </div>
+            </div>
+
+            <div className="container pb-6 m-auto">
+              <Stats color={color}/>
+            </div>
+          </section>
+        </main>
+        <Footer/>
       </div>
     </>
   );
